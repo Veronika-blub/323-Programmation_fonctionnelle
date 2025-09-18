da3ad9349541bb9418601efc14682a67c9d7e11f
Veronika-blub <pu74vys@eduvaud.ch>
2025-09-18 11:58:26 +0200

    mib_reduse

---
+++ VeronikaSkupovska-FID2/4seq-rando/exercice1-randoBase/RandoBase/Rando/Rando/Rando.cs
@@ -0,0 +1,69 @@
+﻿namespace Rando
+{
+    public partial class Rando : Form
+    {
+        string gpxFile = @"..\..\Ballade_châtaignère.gpx";
+        public Rando()
+        {
+            InitializeComponent();
+            List<TrackPoint> points = new();
+            using (GpxReader reader = new GpxReader(streamReader.BaseStream))
+            {
+                while (reader.Read())
+                {
+                    switch (reader.ObjectType)
+                    {
+                        case GpxObjectType.Track:
+                            //writer.WriteTrack(reader.Track);
+                            var gpxPoints = reader.Track.ToGpxPoints();
+
+                            //TODO convertir les gpxPoints en points
+                            //avec un SELECT ;-)
+                            var converted = gpxPoints
+                                .Select(gpxPoint => new TrackPoint()
+                                {
+                                    Elevation = gpxPoint.Elevation,
+                                    Latitude = gpxPoint.Latitude,
+                                    Longitude = gpxPoint.Longitude
+                                });
+
+                            points.AddRange(converted.ToList());
+
+                            break;
+
+                        case GpxObjectType.Metadata:
+                            // writer.WriteMetadata(reader.Metadata);
+                            break;
+                        case GpxObjectType.WayPoint:
+                            // writer.WriteWayPoint(reader.WayPoint);
+                            break;
+                        case GpxObjectType.Route:
+                            // writer.WriteRoute(reader.Route);
+                            break;
+                        case GpxObjectType.Track:
+                            // writer.WriteTrack(reader.Track);
+                            break;
+                    }
+                }
+            }
+
+            /**************************/
+
+            if (!File.Exists(gpxFile))
+            {
+                MessageBox.Show($"Fichier {gpxFile} non trouvé !!");
+            }
+
+     
+        }
+
+        private void Rando_Form_Paint(object sender, PaintEventArgs e)
+        {
+            Pen myPen = new Pen(Color.Red);
+            myPen.Width = 2;
+
+            Point[] points = new Point[4] { new Point(30,50), new Point(50,10), new Point(80,50), new Point(111,400) };
+            this.CreateGraphics().DrawLines(myPen, points);
+        }
+    }
+}
---
+++ VeronikaSkupovska-FID2/4seq-rando/exercice1-randoBase/RandoBase/Rando/Rando/Trackpoint.cs
@@ -0,0 +1,15 @@
+
+
+class TrackPoint
+{
+    private double _latitude;
+    private double _longitude;
+    private double _elevation;
+
+    public TrackPoint(double latitude, double longitude, double elevation)
+    {
+        _latitude = latitude;
+        _longitude = longitude;
+        _elevation = elevation;
+    }
+}
