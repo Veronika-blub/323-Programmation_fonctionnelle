a0a1688d85906b84d6f4f0fe9234f25fcc7d86b1
Veronika-blub <pu74vys@eduvaud.ch>
2025-09-25 10:36:13 +0200

    5seq- randoReduce
    à finir

--- VeronikaSkupovska-FID2/4seq-rando/exercice1-randoBase/RandoBase/Rando/Rando/Rando.cs
+++ VeronikaSkupovska-FID2/4seq-rando/exercice1-randoBase/RandoBase/Rando/Rando/Rando.cs
@@ -1,12 +1,25 @@
-﻿namespace Rando
+﻿using Gpx;
+
+namespace Rando
 {
-    public partial class Rando : Form
+    
+        public partial class Rando : Form
     {
-        string gpxFile = @"..\..\Ballade_châtaignère.gpx";
+        string gpxFile = @"..\..\..\gpx\Ballade_châtaignère.gpx";
+        List<TrackPoint> trackPoints = new();
+
         public Rando()
         {
+
             InitializeComponent();
-            List<TrackPoint> points = new();
+
+            if (!File.Exists(gpxFile))
+            {
+                MessageBox.Show($"Fichier {gpxFile} non trouvé !!");
+            }
+
+            StreamReader streamReader = new StreamReader(gpxFile);
+
             using (GpxReader reader = new GpxReader(streamReader.BaseStream))
             {
                 while (reader.Read())
@@ -14,47 +27,22 @@
                     switch (reader.ObjectType)
                     {
                         case GpxObjectType.Track:
-                            //writer.WriteTrack(reader.Track);
                             var gpxPoints = reader.Track.ToGpxPoints();
 
-                            //TODO convertir les gpxPoints en points
-                            //avec un SELECT ;-)
                             var converted = gpxPoints
                                 .Select(gpxPoint => new TrackPoint()
                                 {
                                     Elevation = gpxPoint.Elevation,
-                                    Latitude = gpxPoint.Latitude,
-                                    Longitude = gpxPoint.Longitude
+                                    Latitude = gpxPoint.Latitude * 10000,
+                                    Longitude = gpxPoint.Longitude * 10000
                                 });
+ 
+                            trackPoints.AddRange(converted.ToList());
 
-                            points.AddRange(converted.ToList());
-
-                            break;
-
-                        case GpxObjectType.Metadata:
-                            // writer.WriteMetadata(reader.Metadata);
-                            break;
-                        case GpxObjectType.WayPoint:
-                            // writer.WriteWayPoint(reader.WayPoint);
-                            break;
-                        case GpxObjectType.Route:
-                            // writer.WriteRoute(reader.Route);
-                            break;
-                        case GpxObjectType.Track:
-                            // writer.WriteTrack(reader.Track);
                             break;
                     }
                 }
             }
-
-            /**************************/
-
-            if (!File.Exists(gpxFile))
-            {
-                MessageBox.Show($"Fichier {gpxFile} non trouvé !!");
-            }
-
-     
         }
 
         private void Rando_Form_Paint(object sender, PaintEventArgs e)
@@ -62,8 +50,36 @@
             Pen myPen = new Pen(Color.Red);
             myPen.Width = 2;
 
-            Point[] points = new Point[4] { new Point(30,50), new Point(50,10), new Point(80,50), new Point(111,400) };
+            //Point[] points = new Point[4] { new Point(30, 50), new Point(50, 10), new Point(80, 50), new Point(111, 400) };
+
+            var minLat = trackPoints.Min(tp => tp.Latitude);
+            var maxLat = trackPoints.Max(tp => tp.Latitude);
+            var rangeLat = maxLat - minLat;
+            var ratioLat = rangeLat / Width;
+
+            var minLong = trackPoints.Min(tp => tp.Longitude);
+            var maxLong = trackPoints.Max(tp => tp.Longitude);
+            var rangeLong = maxLong - minLong;
+            var ratioLong = rangeLong / Height;
+
+            //De trackpoint VERS POINT
+            Point[] points = trackPoints
+                .Select(trackPoint => new Point()
+                {
+                    X = Convert.ToInt32((trackPoint.Latitude - minLat) * ratioLat),
+                    Y = Convert.ToInt32((trackPoint.Longitude - minLong) * ratioLong)
+                }
+                )
+                .ToArray();
+
+
+
             this.CreateGraphics().DrawLines(myPen, points);
         }
-    }
+
+
+
+        //Pour calculer la distance
+        
+    }   
 }
--- VeronikaSkupovska-FID2/4seq-rando/exercice1-randoBase/RandoBase/Rando/Rando/Trackpoint.cs
+++ VeronikaSkupovska-FID2/4seq-rando/exercice1-randoBase/RandoBase/Rando/Rando/Trackpoint.cs
@@ -1,15 +1,38 @@
-
-
-class TrackPoint
+namespace Rando
 {
-    private double _latitude;
-    private double _longitude;
-    private double _elevation;
 
-    public TrackPoint(double latitude, double longitude, double elevation)
+    public class TrackPoint
     {
-        _latitude = latitude;
-        _longitude = longitude;
-        _elevation = elevation;
+
+                private const double EARTH_RADIUS = 6371; // [km]
+        private const double RADIAN = Math.PI / 180;
+        private double _latitude;
+        private double _longitude;
+        private double? _elevation;
+
+
+        /*
+                public TrackPoint(double latitude, double longitude, double elevation)
+                {
+                    _latitude = latitude;
+                    _longitude = longitude;
+                    Elevation = elevation;
+                }*/
+
+        public double? Elevation { get => _elevation; set => _elevation = value; }
+        public double Longitude { get => _longitude; set => _longitude = value; }
+        public double Latitude { get => _latitude; set => _latitude = value; }
+        
+        public double GetDistanceFrom(TrackPoint other)
+        {
+            double thisLatitude = Latitude * RADIAN;
+            double otherLatitude = other.Latitude * RADIAN;
+            double deltaLongitude = Math.Abs(Longitude - other.Longitude) * RADIAN;
+
+            double cos = Math.Cos(deltaLongitude) * Math.Cos(thisLatitude) * Math.Cos(otherLatitude) +
+                Math.Sin(thisLatitude) * Math.Sin(otherLatitude);
+
+            return EARTH_RADIUS * Math.Acos(Math.Max(Math.Min(cos, 1), -1));
+        }
     }
 }
