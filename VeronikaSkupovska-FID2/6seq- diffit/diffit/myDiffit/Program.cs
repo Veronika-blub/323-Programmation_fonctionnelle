9ccd2d93b70abe32abf790f1fe14533c1f725abf
Veronika-blub <pu74vys@eduvaud.ch>
2025-10-30 11:58:58 +0100

    power

--- VeronikaSkupovska-FID2/6seq- diffit/diffit/myDiffit/Program.cs
+++ VeronikaSkupovska-FID2/6seq- diffit/diffit/myDiffit/Program.cs
@@ -1,132 +1,166 @@
-﻿//Auteur : JMY
-//Date   : 25.9.2025
+﻿//Auteur : Veronika Skupovska
+//Date   : 09.10.2025 
 //Lieu   : ETML
-//Descr. : Entraînement au test 323
-//         Cet outil permet de comparer 2 fichiers (avec le même nombre de lignes) ligne par ligne et indiquer les différences... Il permet aussi de faire du chiffrement
+//Descr. : Donnée pour le test pratique 323
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
 
-using System.Runtime.Intrinsics;
-
-///MENU
-Console.WriteLine("+--------------------------------+");
-Console.WriteLine("|DIFFIT : A very limited DIFFTOOL|");
-Console.WriteLine("+--------------------------------+");
-
-Console.Write("Fichier A: ");
-string? pathA = @"C:\Users\pu74vys\Documents\GitHub\323-Programmation_fonctionnelle\VeronikaSkupovska-FID2\6seq- diffit\diffit\v1.txt";  //Console.ReadLine();
-
-Console.Write("Fichier B: ");
-string? pathB = @"C:\Users\pu74vys\Documents\GitHub\323-Programmation_fonctionnelle\VeronikaSkupovska-FID2\6seq- diffit\diffit\v2.txt";  //Console.ReadLine();
-
-// Vérification des entrées utilisateur
-var paths = new string?[] { pathA, pathB };
-bool filesAreValid = paths.Aggregate(true, (a, b) => a && b != null && File.Exists(b));
-if (!filesAreValid)
+namespace KillThoseDuplicatesDonnee
 {
-    Console.WriteLine("Erreur: les fichiers doivent être existants et accessibles !");
-    Environment.Exit(-2);
-}
-
-/// CHARGEMENT DES DONNÉES
-// TODO: 01 Charger le contenu texte du fichier A (indice: File.ReadAllLines...)
-string[] linesA = File.ReadAllLines(pathA);
-
-
-// TODO: 02 Charger le contenu texte du fichier B (indice: File.ReadAllLines...)
-string[] linesB = File.ReadAllLines(pathB);
-
-// TODO: 03 Vérifier que les fichier ont le même nombre de lignes
-if ( linesA.Length != linesB.Length)
-{
-    Console.WriteLine("Erreur: les fichiers n'ont pas le même nombre de ligne");
-    Environment.Exit(-2);
-}
-
-Console.WriteLine(">Fichiers chargés avec succés");
-
-// TODO: 04 Définir les fonctions de nettoyage
-// Une fonction de nettoyage reçoit un texte (une ligne de fichier) et renvoie cette même ligne adaptée
-// Il existe la fonction Replace sur les string...
-// Le caractère tabulation s’écrit \t
-Func<string, string> cleanSpaces = text => text.Replace(" ", "");;
-Func<string, string> cleanTabs = text => text.Replace("\t", "");;
-Func<string, string> enforceCase = text => text.ToLower();;
-
-/// OPTIONS DE NETTOYAGE
-Console.WriteLine("Choisir les options:");
-
-Console.Write("-Ignorer les espaces [o/n]: ");
-bool ignoreSpaces = Console.ReadLine() == "o";
-
-Console.Write("-Ignorer les tabulations [o/n]: ");
-bool ignoreTabs = Console.ReadLine() == "o";
-
-Console.Write("-Ignorer la casse [o/n]: ");
-bool ignoreCase = Console.ReadLine() == "o";
-
-// TODO:  05 Appliquer le nettoyage selon la demande utilisateur
-
-Func<string, string> clean = text =>
-{
-    text = ignoreSpaces ? cleanSpaces(text) : text;
-    text = ignoreTabs ? cleanTabs(text) : text;
-    text = ignoreCase ? enforceCase(text) : text;
-    return text;
-};
-
-linesA = linesA.Select(line => clean(line)).ToArray();
-linesB = linesB.Select(line => clean(line)).ToArray();
-
-// TODO: 06 Créer et remplir une liste de LinesComparison à partir de linesA et linesB
-List<LinesComparison> comparisons = new();
-
-// TODO: 07 Sélectionner les lignes qui ont des différences
-var diffLines = new List<LinesComparison>();
-
-// TODO: 08 Afficher le nombre de lignes identiques et différentes entre les 2 fichiers
-
-// TODO: 09 Définir une fonction qui compte les différences (caractères différents) entre deux textes (sera utilisé pour les 2 lignes de A et B...)
-// Pour info/rappel, la fonction Zip (comme une fermeture éclair) permet d’associer deux listes.
-// Et pour info/rappel, un string est une liste de char...
-// Ainsi "12345".Zip("ABCDE", (a, b) => $"{a}{b}").ToList().ForEach(Console.Write);//1A2B3C4D5E
-// ATTENTION: zip ne prend que le nombre d’éléments minimum commun entre 2 listes...
-// Ceci implique une correction: en plus du nombre de différences, il faut ajouter la différence du nombre de caractères entre les deux...
-Func<LinesComparison, int> countVariations = _ => -1;
-
-// TODO: 10 Afficher pour chaque ligne différente, le nombre de variations
-
-/// Diff coloré
-// TODO: 11 Colorier les différences
-// Pour chaque ligne où il y a des différences:
-// On affiche ainsi:
-// Les lettres similaires sont en vert
-// Les lettres différentes sont en rouge (options entre[a/b])
-// On n’indique rien sur les caractères en plus ou en moins
-
-/// Chiffrement
-// TODO: 11 Créer une fonction qui chiffre le 1er fichier en décalant les caratères d’un nombre
-//saisi par l’utilisateur (clé)
-// Le contenu chiffré est enregistré sur le disque dans le fichier "cipheredA.txt"
-// Le pendant de ReadAllLines est WriteAllLines
-Console.Write("\n\nSPECIAL FEATURE: Clé de chiffrement [1-25]: ");
-byte key = Convert.ToByte(Console.ReadLine());
-
-
-/// <summary>
-/// Classe pour porter une information de comparaison
-/// </summary>
-public class LinesComparison
-{
-    public int Number { get; set; }
-    public string ContentA { get; set; } = "";
-    public string ContentB { get; set; } = "";
-
-    /// <summary>
-    /// Ajuste le numéro de ligne...
-    /// </summary>
-    public int NumberHuman
+    class Program
     {
-        get => Number + 1;
-    }
+        // Le programme KTD sert à repérer les fichiers à double au sein d'une arborescence de dossiers.
+        // Il permet ensuite d'agir sur les doublons:
+        //   - Soit il se contente de les signaler et de proposer des actions
+        //   - Soit il efface les fichiers redondants pour ne garder que celui qui a été utilisé en dernier
+        //
+        // Pour pouvoir dire si deux fichiers sont  identiques ou pas, on se sert d'un discriminant, c'est-à-dire une valeur 
+        // calculée qui est la même pour deux fichiers que l'on considère égaux
+        //
+        // KTD supporte trois manières de déterminer l'égalité de deux fichiers
+        //   1. Deux fichiers sont égaux s'ils ont le même nom
+        //      Exemple: 'C:\Temp\Readme.md' et 'C:\Temp\exo\Readme.md' sont considérés comme égaux
+        //      parce qu'ils ont le même nom ('Readme.md').
+        //
+        //   2. Deux fichiers sont égaux s'ils ont le même nom ET la même taille
+        //      Exemple: 'C:\Temp\Readme.md' et 'C:\Temp\exo\Readme.md' sont considérés comme différents
+        //      parce que le premier fait 1265 bytes (discriminant: 'Readme.md-1265') et le deuxième en fait 975 (discriminant:
+        //      'Readme.md-975')
+        //
+        //   3. Deux fichiers sont égaux s'ils ont le même nom, la même taille ET la même date de modification
+        //      Exemple: 'C:\Temp\Readme.md' et 'C:\Temp\exo\Readme.md' sont considérés comme égaux parce qu'ils font
+        //      la même taille (2319 bytes et ils ont tous les deux été modifiés (LastWriteTime)
+        //      le 1er octobre 2024 (ils ont donc le même discriminant 'Readme.md-2319-2024-10-01')
 
-    public int LengthVariation { get => Math.Abs(ContentA.Length - ContentB.Length); }
+        static void Main(string[] args)
+        {
+			// TODO #0A: (3pts) Faire un commit pour chaque todo 
+			// TODO #0B: (3pts) Respecter les conventions de codage ETML (surtout en terme de nom de variable...)
+			
+			//Affiche l'aide si aucun paramètre n'est fourni
+            if (args.Length == 0)
+            {
+                ShowHelp();
+                return;
+            }
+            string path = args[0];
+			//Initialisation par défaut
+            string currentPath = Directory.GetCurrentDirectory();
+            string extension = "";
+
+            // TODO #1: (3pts) Remplacer les "lambda n'importe quoi" par quelque chose de correct
+            Func<FileInfo, string> ByName = x => x.Name;
+            Func<FileInfo, string> ByNameAndSize = x => $"{x.Name}-{x.Length}";
+            Func<FileInfo, string> ByNameSizeAndDate = x => $"{x.Name}-{x.Length}-{x.LastWriteTime:yyyy-MM-dd}";
+
+            // TODO #2: (3pts) Définir le critère par défaut (en se basant sur la démo ou en proposant un choix pertinent avec commentaire)
+            Func<FileInfo, string> fileCriterion = x => ByName(x); // Par défaut, on considère que deux fichiers sont identiques s'ils ont le même nom
+
+
+            // TODO #3: (3pts) Proposer un discriminant supplémentaire qui tiendrait compte du contenu du fichier
+            // !!! Il suffit d'énoncer une proposition au moyen de commentaires !!! Pas besoin de l'implémenter
+            //FileHash
+
+
+
+
+            // TODO #4: (3pts) Remplacer les "lambda n'importe quoi" par quelque chose de correct et définir l'action par défaut
+            Action<FileInfo> SuggestDelete = x => Console.WriteLine($"A effacer:{x.FullName}");
+            Action<FileInfo> ExecuteDelete = x => { Console.WriteLine($"Effacement de:{x.FullName}"); File.Delete(x.FullName); };
+
+            Action<FileInfo> fileAction = x => SuggestDelete(x); // Par défaut, on suggère les fichiers à effacer sans les effacer réellement
+
+
+            //Traitement des arguments de la ligne de commande
+            // TODO #5: (3pts) Remplacer tous les "lambda n'importe quoi" de ce bloc "for" par quelque chose de cohérent
+            for (int a = 0; a < args.Length; a++)
+            {
+                if (args[a].StartsWith("-h"))
+                {
+                    ShowHelp();
+                    return;
+                }
+                if (args[a].StartsWith("-d")) currentPath = args[a + 1];
+                if (args[a].StartsWith("-e")) extension = "." + args[a + 1];
+                if (args[a].StartsWith("-a"))
+                {
+                    if (args[a + 1] == "echo")
+                        fileAction = x => SuggestDelete(x);
+                    else if (args[a + 1] == "delete")
+                        fileAction = x => ExecuteDelete(x);
+                    else
+                    {
+                        ShowHelp();
+                        return;
+                    }
+                }
+                if (args[a].StartsWith("-c"))
+                {
+                    if (args[a + 1] == "size")
+                        fileCriterion = x => ByNameAndSize(x);
+                    else if (args[a + 1] == "date")
+                        fileCriterion = x => ByNameSizeAndDate(x);
+                    else
+                    {
+                        ShowHelp();
+                        return;
+                    }
+                }
+            }
+            // TODO #6: (2pts) Afficher l'aide en cas d'erreur de paramètres (p.ex: -extension comme dernier paramètre ou en passant -extension sans indiquer l'extension)
+            if (args[args.Length - 1].StartsWith("-e") || args[args.Length - 1].StartsWith("-d") || args[args.Length - 1].StartsWith("-a") || args[args.Length - 1].StartsWith("-c")) { ShowHelp(); return; };
+
+            // TODO #7: (1pts) Terminer le programme si un dossier est donné mais n'existe pas
+            if (!File.Exists(currentPath)) { System.Environment.Exit(1); } ;
+
+            // TODO #8: (6pts) peupler la liste 'fileinfos' en vous aidant des éléments du dossier doc
+            List<FileInfo> fileInfos = Directory.GetFiles(currentPath); // Liste de tous les fichiers dans le dossier choisi et tous ses sous-dossiers
+
+            // TODO #9: (6pts) Si une extension a été donnée, ne garder que les fichiers qui correspondent
+
+            // TODO #10: (9pts) Identifier les fichiers à double (ou plus) en passant par une structure <string,List<FileInfo>> où string est le discriminant (p.ex: 'Readme.md-123456')
+
+            // TODO #11: (9pts) Traiter les duplicats avec l'action choisie
+
+            Console.ReadKey();
+        }
+
+        static private void ShowHelp()
+        {
+            Console.WriteLine("KillThoseDuplicates (ktd) par VeronikaSkupovska:");
+            Console.WriteLine(
+                "Un utilitaire qui permet de gérer les fichiers en double dans une arborescence\n" +
+                "\n" +
+                "usage: ktd [-h[elp]] [-e[xtension] <ext>] [-d[irectory] <path>] [-a[ction] {echo|delete}] [-c[riterion] {size|date}]\n" +
+                "\n" +
+                "-d[irectory]:  s'applique au dossier <path>. Le répertoire courant s'il n'est pas spécifié\n" +
+                "-e[xtension]:  ne s'applique qu'aux fichiers qui ont l'extension est <ext>\n" +
+                "-c[riterion]:  size: considère deux fichiers identiques par nom ET taille\n" +
+                "               date: considère deux fichiers identiques par nom, taille ET date de modification\n" +
+                "-a[ction]:     echo: affiche les fichiers à effacer\n" +
+                "               delete: effectue l'effacement des fichiers (et l'annonce)"
+                );
+            Console.ReadKey();
+        }
+
+        /*==================================================================================================================
+         
+        Aides:
+
+        - La classe Program est statique. Les méthodes que vous déclarez doivent donc également être statiques
+
+        - Pour connaître les attributs d'un objet de type FileInfo, faire F12 ou l'inspecter avec le debugger
+
+        - Pour passer des arguments à votre programme pendant le développement dans Visual Studio:
+            Menu Déboguer>Propriétés de débogage
+
+		- Pour passer des arguments via la ligne de commande, on peut procéder ainsi (-- après run):
+		  dotnet run -- -e txt -d data -a echo -c date
+
+        - Appliqué à la structure de fichiers de test fournis (dossier ‘data’)
+            - Sans préciser de critère: KTD agit sur les fichiers dans les trois répertoires 'copy','bigger' et 'older'
+            - Avec '-criterion size': KTD n'agit que sur les fichiers de 'copy' et 'older' 
+            - Avec '-criterion date': KTD n'agit que sur les fichiers de 'copy'
+        ==================================================================================================================*/
+    }
 }
---
+++ VeronikaSkupovska-FID2/8seq-puissance/puissances/puissances/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+static int Puissance(int nombre, int exposant)
+{
+    if (exposant == 0) return 1;
+    
+    return nombre * Puissance(nombre, exposant - 1);
+}
+Console.WriteLine(Puissance(2, 3));
+
+
+//  static int Fibonacci(int n)
+// {
+//     int n1 = n;
+//      if (n > 100) return n;
+
+//     return n + Fibonacci(n);
+     
+//  }
+//  Console.WriteLine(Fibonacci(2));
+
+static List<int> Fibonacci(int n)
+{
+    var seq = new List<int>();
+    if (n <= 0) return seq; 
+    seq.Add(0);
+    if (n == 1) return seq;
+    seq.Add(1);
+    for (int i = 2; i < n; i++)
+        seq.Add(seq[i - 1] + seq[i - 2]);
+    return seq;
+}
+Console.WriteLine(string.Join(", ", Fibonacci(0)));
